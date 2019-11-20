47ddea9c1b6ca10df37c9cdcbc8a7e86c90b6225
tientom2017 <leyentien96@gmail.com>
2019-11-20 22:41:27 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Exam from "./folder/exam.js";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            color: 'red'
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <p style={{color: this.state.color}}>heading 1</p>
+                `12`12`12
+                <Exam color={this.state.color} />
+            </div>
+        );
+    };
+    
+}
+
+
+export default App;
---
+++ src/folder/exam.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Exam extends Component{
+    render() {
+        return(
+            <div><p>asdsdasdssasds</p></div>
+        );
+    }
+}
+
+export default Exam;
